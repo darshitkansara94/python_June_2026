792f72a208ef06b76962fcf6141e313fadbf06e4
darshitkansara94 <48837243+darshitkansara94@users.noreply.github.com>
2026-06-25 19:43:41 +0530

    Variables
    Variables

---
+++ Variables.py
@@ -0,0 +1,45 @@
+# Variables :
+#     Variables are use to store a value.
+#     And that value we can store through out the program whenever we need by
+#         using variable name.
+#     In python variable name is case sensitive.
+#     We can not declare a same varible name in same editor.
+#     Variable name does not contain space.
+#     Variable name should not start with special char.
+
+    # Syntax :
+        # variable_name = value
+
+    # Example :
+x = 5
+y = "Hi"
+print("John say " + y)
+print("Dev say " + y)
+print(type(x)) # Identify the datatype of variable
+print(type(y)) # Identify the datatype of variable
+
+z = 'Hello'
+print(type(z)) # Identify the datatype of variable
+
+# firstName = "Jay say's Hello"
+# print(firstName)
+# name = 'Jay say's Hello'
+# print(name)
+print(x)
+X = "Hello"
+print(x)
+print(X)
+
+# Convert a value from one type to another
+value1 = str(10) 
+print(type(value1))
+
+value2 = str(10.25)
+print(type(value2))
+print(value2)
+
+FirstName = "Darshit"
+LastName = "Kansara"
+MoNo = 123456789
+print(FirstName + LastName + str(MoNo))
+print(type(MoNo))
