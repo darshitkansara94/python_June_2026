e3750809233567d3f7c7e6e61eaa8c4b3ee126bf
darshitkansara94 <48837243+darshitkansara94@users.noreply.github.com>
2026-06-30 19:35:51 +0530

    Date 30 June 2026
    Datatypes

--- Datatype.py
+++ Datatype.py
@@ -64,3 +64,91 @@ print(str_Fullstring[:7])
 print(len("Hello World"))
 # print(Len("Hello World")) # Display runtime error because in Len function name we 
     # have written 'L' as a capital.
+
+# Concate :
+    # Concatenation is merging two or more than two words and present as a single
+    #     string value.
+
+    # Example :
+print("Display Concetenate example")
+Fullname = "Darshit" + " " + "Kansara"
+print(Fullname)
+
+Firstname = "Darshit"
+Lastname = "Kansara"
+FullnameByVariable = Firstname+ " " + Lastname
+print(FullnameByVariable)
+
+fullname = "Darshit Kansara"
+Age = 25
+print(type(fullname))
+print(type(Age))
+# FullNameAndAge = fullname + Age
+# print(FullNameAndAge)
+
+# Format :
+    # Merge srting and display as a single string value.
+    # This function is use to display a dynamic message to the user or some 
+    #   string value.
+    # Format function always start with char 'f'.
+
+    # Syntax :
+        # variable_name = f"string value"
+
+    # Example :
+lastname = "Kansara"
+Name = f"Darshit {lastname}"
+print(Name)
+
+AgePrint = f"Person age is : {Age}"
+print(AgePrint)
+
+FullNameWithAge = f"Person Name is {Firstname} {Lastname} and age is {Age}"
+print(FullNameWithAge)
+# FullnameWithAgeandplus = "Person name is " + Firstname + Lastname  + " Age is " + str(Age)
+
+# Capitalize :
+#     Capitalization use to display first char in uppercase and remain in lower case.
+
+    # Syntax :
+        # print(variable_name.capitalize())
+
+    # Example :
+helloworld = "hello WORLD"
+print(helloworld.capitalize())
+
+# Lower() :
+    # Convert a string into lower case.
+
+    # Syntax :
+        # print(variable_name.lower())
+
+    # Example :
+print(helloworld.lower())
+
+# Upper() :
+    # Convert  a case into upper case
+
+    # Syntax :
+        # varibale_name.upper()
+
+    # Example :
+print(helloworld.upper())
+
+# split :
+    # split function divide a string in diff part and present in array.
+    # To seperate a string we need to use seperator.
+    # By default seperator is space.
+
+    # Syntax :
+        # variable_name.split()
+
+    # Example :
+print(FullnameByVariable.split())
+
+x = "Good evening, Welcome to class"
+print(x.split(","))
+print(x.split())
+
+y = "This is montain. Mountain is good"
+print(y.split("is",1))
