0a84a1daec407e608845996f8090223ee89f41f8
darshitkansara94 <48837243+darshitkansara94@users.noreply.github.com>
2026-08-04 19:55:27 +0530

    Date 04 Aug 2026
    Function revision

---
+++ Functions_Example_1.py
@@ -0,0 +1,61 @@
+def greetings(message):
+    print(message)
+
+greetings("Good morning")
+greetings("Good evening")
+
+# def greetings():
+#     print("Good afternoon")
+
+# def greetings():
+#     print("Good evening")
+
+# def greetings():
+#     print("Good night")
+
+def Multiplication(value1,value2):
+    return value1 * value2
+
+x = int(input("Enter value1 : ")) # 10
+y = int(input("Enter value2 : ")) # 20
+z = int(input("Enter value3 : "))
+mul = Multiplication(x,y) #Multiplication(10,20)
+
+print(mul + z)
+
+def MultiplicationWithoutParam():
+    input1 = int(input("Enter value1 : "))
+    input2 = int(input("Enter value2 : "))
+    input3 = int(input("Enter value3 : "))
+
+    return input1 * input2 + input3
+
+print(MultiplicationWithoutParam())
+
+if(time > 7 and time <12)
+print("morning")
+elif time >12 and time <5
+print(noon)
+elif time >5 and time <8
+print(evening)
+else
+print(good night)
+
+greetingMessge = ""
+if(time > 7 and time <12)
+greetingMessge = "Good mornig"
+elif time >12 and time <5
+greetingMessge = "Good noon"
+elif time >5 and time <8
+greetingMessge = "Good evening"
+else
+greetingMessge = "Good night"
+
+greetings(greetingMessge)
+
+greetings("Hello How are you?")
+
+# Assignment :
+    # Create a common function that perform Addition, Multiplication, Division and Substraction.
+    # Based on user input this operation should perform.
+    # This code should execute until user select exit option.
