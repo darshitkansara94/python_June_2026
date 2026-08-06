dd857e52dcd69d0944e42bef7021eea4a2770230
darshitkansara94 <48837243+darshitkansara94@users.noreply.github.com>
2026-08-06 19:28:42 +0530

    Date 06 Aug 2026
    Math function
    Datetime

--- Datetime.py
+++ Datetime.py
@@ -15,3 +15,47 @@ print(currentYear)
 
 onlyyear = datetime.datetime.now().year
 print(onlyyear)
+
+# Print a date
+day = currentdatetime.strftime('%d')
+print(day)
+
+month = currentdatetime.strftime('%B')
+print(month)
+
+month_num = currentdatetime.strftime('%b')
+print(month_num)
+
+time = currentdatetime.strftime('%H')
+print(time)
+
+second = currentdatetime.strftime('%M')
+print(second)
+
+# How to get a next value of date
+increaseDay = currentdatetime + datetime.timedelta(days = 1)
+print(increaseDay)
+
+increaseYear = currentdatetime.replace(year = currentdatetime.year + 3)
+print(increaseYear)
+
+increaseMonth = currentdatetime.replace(month = currentdatetime.month + 3)
+print(increaseMonth)
+
+decreaseDay = currentdatetime - datetime.timedelta(days = 3)
+print(decreaseDay)
+
+decreaseYear = currentdatetime.replace(year = currentdatetime.year - 3)
+print(decreaseYear)
+
+decreaseMonth = currentdatetime.replace(month = currentdatetime.month - 3)
+print(decreaseMonth)
+
+increaseTime = currentdatetime + datetime.timedelta(hours = 2)
+print(increaseTime)
+
+increaseMinute = currentdatetime + datetime.timedelta(minutes = 2)
+print(increaseMinute)
+
+increasedateAndTime = currentdatetime + datetime.timedelta(days = 1,hours = 2)
+print(increasedateAndTime)
---
+++ Math_Function.py
@@ -0,0 +1,47 @@
+# Math function :
+#     Math function are use to perform mathemetical operations. 
+#     Python already support some basic mathemetical operations.
+
+#     Types of Math function :
+#         min :
+#           Minimum value is use to find a minimum value from the tuple.
+
+#           Syntax :
+#               min(valu1,value2,value3,...,valueN)
+#           
+#           Example :
+x = (10,20,25,1,45)
+print(min(x))
+#         max :
+#           Maximum value is use to find a maximum value from the tuple.
+
+#           Syntax :
+#                max(valu1,value2,value3,...,valueN)
+
+#           Example :
+print(max(x))
+
+#         Absolute :
+#           Absolute is use to convert negative value into positive value.
+#           Absolute function we can use with the keyword 'abs'
+
+#           Syntax :
+#               abs(value)
+
+#           Example :
+y = abs(-7.55)
+print(y)
+
+#         Power :
+#           power is exponential of the value.
+#           We can use power function with the keyword 'pow'
+#              
+#           Syntax :
+#                pow(base,exponential,[modulos])
+
+#           Example :
+z = pow(2,4)
+print(z)
+
+a = pow(2,4,2)
+print(a)
