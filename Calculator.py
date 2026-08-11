a576f9573c6989aa02f7f21d4ef66aa15e435957
darshitkansara94 <48837243+darshitkansara94@users.noreply.github.com>
2026-08-11 19:34:03 +0530

    Date 11 Aug 2026
    OOPs (Class and Objects)

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+class Calculator():
+    x = 3  
+
+    def Addition(self):
+          print(10 + 20)    
+
+    def Multiplication():
+         print(10 * 10)
+
+    def Division():
+         print(10 / 10)
+
+p1 = Calculator()
+print(p1.x)
+# Calculator.Addition()
+p1.Addition()
+# p1.Multiplication()
+# p1.Division()
+
+p2 = Calculator()
+p2.Addition()
---
+++ OOPs.py
@@ -0,0 +1,28 @@
+# OOPs :
+#     OOPs is a programming concept which allow developer to do
+#         programming in better way and approach.
+#     Full name of OOPs is Object Oriented Programming.
+#     By using this concept we can create a code that is reusable and
+#         readable.
+#     If code is reusable than we can avoid duplication of code which give more clear 
+#         undertanding of code.
+#     OOPs basically have concept called class and Objects.
+
+#     Class :
+#         Class is a collection of variable and methods
+
+#         Syntax :
+#             class class_name():
+#                 variables
+#                 Methods / function
+#     Object :
+#         Object is a replication of class which allow us to access property and 
+#             methods of the class.
+#         We can create multiple object of the single class.
+
+#         Syntax :
+#             variable_name = class_name()
+
+
+
+
